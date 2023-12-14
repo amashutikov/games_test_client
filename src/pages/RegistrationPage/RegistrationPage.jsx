ba25974edde1a20c9f004822cf665d5e04e9c12b
amashutikov <mashutikov.a.o@gmail.com>
2023-12-14 15:19:41 +0200

    50%

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RegistrationPage.scss';
 import Input from '@mui/joy/Input';
 import Stack from '@mui/joy/Stack';
 import InfoOutlined from '@mui/icons-material/InfoOutlined';
-import FormHelperText from '@mui/joy/FormHelperText';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export const RegistrationPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleToggleConfirmation = () => {
+    setShowConfirmation(!showConfirmation);
+  };
+
+  const handleTouchToggle = (setter) => () => {
+    setter((prev) => !prev);
+  };
+
+  const getEndDecorator = (show, toggleHandler) => {
+    return show ? (
+      <VisibilityOffIcon
+        className='input_hide'
+        onMouseDown={toggleHandler}
+        onMouseUp={toggleHandler}
+        onTouchStart={handleTouchToggle(toggleHandler)}
+        onMouseOut={() => toggleHandler(false)}
+      />
+    ) : (
+      <RemoveRedEyeIcon
+        className='input_hide'
+        onMouseDown={toggleHandler}
+        onMouseUp={toggleHandler}
+        onTouchStart={handleTouchToggle(toggleHandler)}
+        onMouseOut={() => toggleHandler(false)}
+      />
+    );
+  };
+
   return (
     <div className='register'>
       <form className='register__form'>
@@ -13,27 +49,46 @@ export const RegistrationPage = () => {
 
         <Stack spacing={2}>
           <label className='register__label'>Please enter your email</label>
-          <Input placeholder='Email' required />
-          <FormHelperText>
+          <Input placeholder='Email' required size='lg' />
+          <p className='register__message hidden'>
             <InfoOutlined />
-            Opps! something is wrong.
-          </FormHelperText>
+            Oops! something is wrong.
+          </p>
 
           <label className='register__label'>Please enter your password</label>
-          <Input placeholder='Password' required />
-          <FormHelperText>
+          <Input
+            placeholder='Password'
+            required
+            type={showPassword ? 'text' : 'password'}
+            size='lg'
+            endDecorator={getEndDecorator(showPassword, handleTogglePassword)}
+          />
+          <p className='register__message hidden'>
             <InfoOutlined />
-            Opps! something is wrong.
-          </FormHelperText>
+            Oops! something is wrong.
+          </p>
 
-          <label className='register__label'>Please confirm your passwoord</label>
-          <Input placeholder='Confirm password' required />
-          <FormHelperText >
+          <label className='register__label'>
+            Please confirm your password
+          </label>
+          <Input
+            placeholder='Confirm password'
+            required
+            type={showConfirmation ? 'text' : 'password'}
+            size='lg'
+            endDecorator={getEndDecorator(
+              showConfirmation,
+              handleToggleConfirmation
+            )}
+          />
+          <p className='register__message hidden'>
             <InfoOutlined />
-            Opps! something is wrong.
-          </FormHelperText>
+            Oops! something is wrong.
+          </p>
 
-          <button type='submit' className='register__button'>Register</button>
+          <button type='submit' className='register__button'>
+            Register
+          </button>
         </Stack>
       </form>
     </div>
