98fb50d2612c64d54390b5628b759d1428789d60
amashutikov <mashutikov.a.o@gmail.com>
2024-01-11 14:54:36 +0200

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header/Header';
-// import { GamesPage } from './pages/GamesPage/GamesPage';
+import { GamesPage } from './pages/GamesPage/GamesPage';
 import { HomePage } from './pages/HomePage/HomePage';
 import './App.css';
 import { RegistrationPage } from './pages/RegistrationPage/RegistrationPage';
@@ -9,11 +9,9 @@ import { Footer } from './components/Footer/Footer';
 import { Routes, Route } from 'react-router-dom';
 import { RegisterSuccessPage } from './pages/RegisterSuccessPage/RegisterSuccessPage';
 import { ActivatePage } from './pages/ActivatePage/ActivatePage';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 
-// import { AccountActivationPage } from './pages/AccountActivationPage';
 // import { AuthContext } from './components/AuthContext';
-// import { LoginPage } from './pages/LoginPage';
-// import { RegistrationPage } from './pages/RegistrationPage';
 // import { RequireAuth } from './components/RequireAuth';
 // import { UsersPage } from './pages/UsersPage';
 // import { Loader } from './components/Loader.jsx';
@@ -26,14 +24,17 @@ function App() {
       <main className='main'>
         <Routes>
           <Route path='/registration' element={<RegistrationPage />} />
+          
+          <Route path='/login' element={<LoginPage />} />
 
           <Route path='/successregister' element={<RegisterSuccessPage />} />
 
           <Route path='/activate/:token' element={<ActivatePage /> } />
 
+          <Route path='/games' element={<GamesPage />} />
+
           <Route path='/' element={<HomePage />} />
         </Routes>
-        {/* <GamesPage /> */}
       </main>
       <Footer />
     </>
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoginPage.scss';
+
+export const LoginPage = () => {
+  return (
+    <div className='page'>
+      <h1>Login page</h1>
+    </div>
+  );
+};
