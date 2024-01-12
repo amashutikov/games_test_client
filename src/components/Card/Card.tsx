7954eb5b0c758b47e665592edd6069652ffc1cde
amashutikov <mashutikov.a.o@gmail.com>
2024-01-12 13:23:22 +0200

    fixed webpack build problems

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,19 +5,20 @@ type Props = {
   game: Game;
 };
 
-const BASE_IMAGE_SRC = 'https://images.igdb.com/igdb/image/upload/t_720p/'
+const BASE_IMAGE_SRC = 'https://images.igdb.com/igdb/image/upload/t_720p/';
 
 export const Card: React.FC<Props> = ({ game }) => {
-  console.log(game);
 
   return (
     <li className='card'>
-      <img src={`${BASE_IMAGE_SRC + game.artworks[0].image_id}.jpg`} className='card_image' />
+      <img
+        src={`${BASE_IMAGE_SRC + game.artworks[0].image_id}.jpg`}
+        className='card_image'
+      />
       <div className='card_about'>
         <h4 className='card_title'>{game.name}</h4>
         <p className='card_description'>
-          {game.summary.length > 0 &&
-            game.summary.slice(0, 118) + '...'}
+          {game.summary.length > 0 && game.summary.slice(0, 118) + '...'}
         </p>
       </div>
     </li>
