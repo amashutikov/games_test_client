f71170868ce8a50a34aa80bb70c35b4410b22764
amashutikov <mashutikov.a.o@gmail.com>
2024-01-31 17:49:48 +0200

    added getTopGames func

--- src/api/games.ts
+++ src/api/games.ts
@@ -1,12 +1,12 @@
+import { Game } from '../types/Game';
 import { client } from '../utils/fetchClient';
 
 export const getGames = (page: number, genre: string) => {
   const data = `offset ${page * 24}; 
     fields name, summary, id, slug, artworks.*, cover.*;
     limit 24;
-    sort rating desc;
     where summary != null 
-    & platforms = 6
+    & release_dates.platform = (6)
     & category = 0
     & artworks != null 
     & themes != (42) 
@@ -14,6 +14,79 @@ export const getGames = (page: number, genre: string) => {
     ${genre !== 'All genres' ? '& genres.name = ' + `"${genre}"` : ''};`;
 
   return client.get(data, '/games');
+};
+
+export const getTopGames = async (numberOfGames: number) => {
+  type Data = {
+    id: number;
+    total_rating: number;
+    total_rating_count: number;
+  };
+
+  let countOfGames = 0;
+  let numberOfFetches = 0;
+  let limit = 500;
+  let offset = 0;
+  const data: Data[] = [];
+  const bodyQuery = `where summary != null 
+    & category = 0
+    & artworks != null 
+    & themes != (42) 
+    & cover != null
+    & release_dates.platform = (6);`;
+
+  await client.get(`${bodyQuery}`, '/games/count').then((res) => {
+    countOfGames = res.count;
+    numberOfFetches = Math.ceil(countOfGames / 500);
+  });
+
+  for (let i = 0; i < numberOfFetches; i++) {
+    await client
+      .get(
+        `fields id, total_rating, total_rating_count;
+        limit ${limit};
+        offset ${offset};
+        sort total_rating desc;
+        ${bodyQuery}`,
+        '/games'
+      )
+      .then((res) => data.push(...res));
+
+    if (countOfGames - offset < 500) {
+      limit = countOfGames - offset;
+      continue;
+    }
+    offset += 500;
+  }
+
+  const rated = data
+    .map((item) => {
+      const averageRating = item.total_rating;
+      const numberOfRatings = item.total_rating_count;
+      const weightedRating =
+        (numberOfRatings / (numberOfRatings + 100)) * averageRating;
+
+      return {
+        weightedRating,
+        id: item.id,
+      };
+    })
+    .sort((a, b) => b.weightedRating - a.weightedRating);
+
+  const top: Game[] = await client.get(
+    `fields name, summary, id, slug, artworks.*, cover.*;
+    sort total_rating desc;
+    limit ${numberOfGames};
+    where id = (${rated
+      .slice(0, numberOfGames)
+      .map((item) => item.id)
+      .join(',')});`,
+    '/games'
+  );
+
+  console.log(rated);
+
+  return top;
 };
 
 export const getTopRatedGames = () => {
@@ -28,12 +101,12 @@ export const getTopRatedGames = () => {
     & cover != null;`;
 
   return client.get(data, '/games');
-}
+};
 
 export const getAmountOfGames = (genre: string) => {
   const data = `
   where summary != null 
-  & platforms = 6
+  & release_dates.platform = (6)
   & category = 0
   & artworks != null 
   & themes != (42) 
--- src/components/CardCarousel/CardCarousel.tsx
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -5,14 +5,19 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
+import { getTopGames } from '../../api/games';
+import { Game } from '../../types/Game';
 
 export const CardCarousel = () => {
   const [, setSearchParams] = useSearchParams();
   const [carouselPosition, setCarouselPosition] = useState(0);
+  const [games, setGames] = useState<Game[]>([]);
 
-  useEffect((
-    
-  ) => {}, []);
+  const totalCards = 24; // Adjust this to the total number of cards
+
+  useEffect(() => {
+    getTopGames(totalCards).then((res) => setGames(res));
+  }, []);
 
   const navigate = useNavigate();
 
@@ -25,7 +30,6 @@ export const CardCarousel = () => {
   };
 
   const handleArrowClick = (direction: 'left' | 'right') => {
-    const totalCards = 8; // Adjust this to the total number of cards
     const maxPosition = (totalCards - 3) * -312; // Calculate the maximum position
 
     if (direction === 'left' && carouselPosition === 0) {
@@ -45,38 +49,6 @@ export const CardCarousel = () => {
     navigate('/games');
   };
 
-  const game = {
-    id: 88973,
-    artworks: [
-      {
-        id: 14689,
-        alpha_channel: false,
-        animated: false,
-        game: 88973,
-        height: 1080,
-        image_id: 'arbc1',
-        url: '//images.igdb.com/igdb/image/upload/t_thumb/arbc1.jpg',
-        width: 1920,
-        checksum: 'ef4ff28f-dcf4-dc07-79eb-7c2cc08a7d8a',
-      },
-    ],
-    cover: {
-      id: 92497,
-      alpha_channel: false,
-      animated: false,
-      game: 88973,
-      height: 800,
-      image_id: 'co1zdd',
-      url: '//images.igdb.com/igdb/image/upload/t_thumb/co1zdd.jpg',
-      width: 600,
-      checksum: '4519fbae-714a-5b81-17cf-151df9907998',
-    },
-    name: 'Goblin Sword',
-    slug: 'goblin-sword',
-    summary:
-      'Goblin Sword is a retro-inspired action platformer with light rpg elements.\n\nAn army of monsters led by an evil wizard have invaded your hometown. Slay as many monsters as you can, collect loot, avoid dangerous traps and defeat menacing bosses, before facing the evil wizard himself.\n\nReviews:\n“It\'s such a crazy value and has such good production quality it kind of speaks for itself.” 5/5 TouchArcade\n"Fun visuals, good music, engaging level design, and lots of content make Goblin Sword an excellent little game." Editor\'s Choice -148 Apps\n“A deeply compelling and staggeringly impressive tribute to retro games.” 10/10 ArcadeLife\n\nFeatures:\n-89 levels\n-13 bosses\n-30 weapons with unique special attacks\n-30 relics that grant you abilities\n-14 costumes\n-8 guardians that follow you around and assist you\n-5 secret very hard levels\n-Decorate your home with souvenirs\n-Customizable touch controls\n-Universal app. Works on iPad, iPhone and iPod touch.\n-iCloud and MFi support\n-Game Center achievements and leaderboards\n-Premium game. No IAP or ads ever.',
-  };
-
   return (
     <div className='carousel'>
       <div className='carousel__container'>
@@ -91,35 +63,16 @@ export const CardCarousel = () => {
             className='carousel__slide'
             style={{ transform: `translateX(${carouselPosition}px)` }}
           >
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
-            <div>
-              <Card onClick={handleCardClick} game={game} />
-            </div>
+            {games.map((game) => (
+              <div key={game.id}>
+                <Card onClick={handleCardClick} game={game} />
+              </div>
+            ))}
           </div>
         </div>
         <div
           className={`carousel__right ${
-            carouselPosition <= (8 - 3) * -312 ? 'disabled' : ''
+            carouselPosition <= (totalCards - 3) * -312 ? 'disabled' : ''
           }`}
           onClick={() => handleArrowClick('right')}
         >
