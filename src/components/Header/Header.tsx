56c9c4e1dffb808a0f0ad80e81fa308894e69f1f
amashutikov <mashutikov.a.o@gmail.com>
2024-02-08 17:42:50 +0200

    menu bar ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,24 @@ import { Link, useLocation } from 'react-router-dom';
 import { Logo } from '../Logo/Logo';
 import './Header.scss';
 import { VscMenu, VscAccount } from 'react-icons/vsc';
+import { jwtDecode } from 'jwt-decode';
 
 export const Header = () => {
   const location = useLocation();
+
+  let user: { email: string; id: string } | null = null;
+  const accessTokenCookie = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('accessToken'));
+
+  if (accessTokenCookie) {
+    const accessToken = accessTokenCookie.split('=')[1];
+
+    const decodedToken: { email: string; id: string } = jwtDecode(accessToken);
+
+    user = { email: decodedToken.email, id: decodedToken.id };
+  }
+
   return (
     <div className='header'>
       <Logo />
@@ -44,7 +59,7 @@ export const Header = () => {
         <div className='header__authorization header__authorization-authorized'>
           {/* add header__authorization-authorized class if the user is authorized */}
           <VscAccount color='white' size={20} />
-          w@w.w
+          {user ? user?.email : <Link to={'/registration'}>Sign In</Link>}
         </div>
       </Link>
     </div>
