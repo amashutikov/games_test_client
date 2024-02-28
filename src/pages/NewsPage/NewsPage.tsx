36b73bfc23f6d3eda8e5596d7dd7e62ca546766f
amashutikov <mashutikov.a.o@gmail.com>
2024-02-28 13:15:23 +0200

    added news component loader

--- src/pages/NewsPage/NewsPage.tsx
+++ src/pages/NewsPage/NewsPage.tsx
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getNewsById } from '../../api/news';
 import { NewsComponent } from '../../components/NewsComponent/NewsComponent';
+import { News } from '../../types/News';
 import './NewsPage.scss';
+import { Loader } from '../../components/Loader/Loader';
 
 export const NewsPage = () => {
+  const [news, setNews] = useState<News | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+
+  const { newsId } = useParams();
+
+  useEffect(() => {
+    console.log(loading);
+
+    if (!newsId) {
+      return;
+    }
+
+    getNewsById(newsId).then((res) => {
+      setNews(res);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <div className='news_page'>
-      <NewsComponent />
+      {loading && <Loader />}
+      {news && <NewsComponent news={news} />}
     </div>
   );
 };
